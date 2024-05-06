941520ccc75e6e085c2979faa0b708524de5c860
Julie Schult <julie@schult.no>
2024-05-06 10:46:00 +0200

    add simple preprocessing function

--- preprocessing/splitpadsave.py
+++ preprocessing/splitpadsave.py
@@ -116,3 +116,60 @@ def splitpad(csv_path, TRAIN_ID, VAL_ID, TEST_ID, padding_type, augment, length,
     else:
         return X_train, y_train, X_val, y_val, X_test, y_test, n_test
     
+    
+# FOR ONLY PREPROCESS ONE CSV FILE (ex. by testing of another dataset)
+
+def splitpad_simple(csv_path, ID, padding_type, augment, length, save):
+    df = pd.read_csv(csv_path)
+    df_id = pd.read_csv(ID)
+
+    ids = df_id['id'].tolist()
+
+    # X=tensor, y=label, n=name
+    X = []
+    y = []
+    n = []
+
+    for _, row in df.iterrows():
+        id = row['id']
+        path = row['path']
+        label = row['label']
+
+        waveform, _ = torchaudio.load(path)
+        
+        if augment == 0:
+            waveforms = [waveform]
+            extra_names = ['originalClipped']
+        if augment == 1:
+            waveforms = speedPert(waveform, 0.9, 1.1)
+            extra_names = ['originalClipped', 'augmentedClipped']
+        if augment == 2:
+            waveforms = speedPert2(waveform, 0.9, 1.1)
+            extra_names = ['originalClipped', 'augmentedClipped', 'augmentedClipped2']
+
+        for idx, w in enumerate(waveforms):
+            if (len(np.array(w.squeeze(0)))) > (int(length*16000)):
+                all_clips, all_names = split_audios(file_path=path, extra_name=extra_names[idx], limit_s=10)
+                for idx0, val in enumerate(all_clips):
+                    val = padding(val, length_seconds=length, padding_type=padding_type)
+                    if id in ids:
+                        X.append(val)
+                        y.append(label)
+                        n.append(all_names[idx0])
+            else:
+                waveform_padded = padding(w, length_seconds=length, padding_type=padding_type)
+                if idx in (1, 2):
+                    path = path[:-4] + '_augmented_0'
+                    
+                if id in ids:
+                        X.append(waveform_padded)
+                        y.append(label)
+                        n.append(path)
+    
+    if save == 'pth':
+        torch.save(X, 'X.pth')
+        torch.save(y, 'y.pth')
+        torch.save(n, 'n.pth')
+        return
+    else:
+        return X, y, n
